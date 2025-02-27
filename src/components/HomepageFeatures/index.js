5ad51cf2d5622925e58a074fb9dc751c4fb8594b
dangalcan <megamagolas@gmail.com>
2025-02-27 16:16:52 +0100

    doc: add basic API documentation structure

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,6 +8,26 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
+      This tool allows you to migrate a service chain from iTop and Redmine without information loss.
+      The Wizard uses the SC Model to store the information of the service chain.
+      </>
+    ),
+  },
+  {
+    title: 'What is SCModel?',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+    <>
+      The SC Model (Service Chain Model) is a YAML file modelling language that contains a standardized description of a service chain.  
+      See more details in <a href="http://lucidchain.specs.governify.io" target="_blank" rel="noopener noreferrer">SCModel official documentation</a>.
+    </>
+    ),
+  },
+  {
+    title: 'What is Lucid Chain Wizard?',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
